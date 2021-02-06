1575b63a981be72f5484bc8e316a7bb049179de1
yssefnoam <2020noam@gmail.com>
2021-02-06 18:38:36 +0000

    a

---
+++ db2/1.sql
@@ -0,0 +1,23 @@
+drop database info1;
+
+create database info1;
+
+use info1;
+
+create table orders(
+	orderId int auto_increment,
+	orderDate date,
+	constraint orders_pk
+	primary key(orderId)
+);
+
+create table ordersItem(
+	orderId int not null,
+	orderName varchar(26),
+	order_Description varchar(150),
+
+	constraint ordersItem_fk
+	foreign key(orderId)
+	references orders(orderId)
+);
+

6e1ba33d091d296b1506581f88bff6c5a8c5c0b0
yssefnoam <2020noam@gmail.com>
2021-02-06 18:13:28 +0000

    a

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # learnMySql
-## To enter mysql
-mysql -u root -p
-## to change mysql password
-mysqladmin --user=root --password=oldpassword password "newpassword"
--- addData.sql
+++ addData.sql
@@ -6,7 +6,7 @@ use company; -- enter to company database to configure
 
 -- create table in database company
 create table employee( -- table name employee
-	empid int primary key, -- employee id ; type int ; this primary key is unique must not duplicated
+	empid int auto_increment primary key, -- employee id ; type int ; this primary key is unique must not duplicated
 	firstName varchar(25) not null, -- firstname of the employee; type string(varchar); max charactees 25, required to fill
 	lastName varchar(25) not null,   -- lastname of the employee; type string(varchar); max charactees 25, required to fill
 	salary DECIMAL(5,2)check(salary>100), -- check if the person have greather than 100$ in salary; type float; #####.##
@@ -16,14 +16,12 @@ create table employee( -- table name employee
 describe employee; -- show the the structure of the employee table
 
 insert into employee( -- insert employee 1 in the employee table 
-	empid,
 	firstName,
 	lastName,
 	salary,
 	dep
 	)
 	values (
-	1,
 	"youssef",
 	"noam",
 	100.22,
@@ -31,21 +29,19 @@ insert into employee( -- insert employee 1 in the employee table
 );
 
 insert into employee( -- insert employee 2 in the employee table 
-	empid,
 	firstName,
 	lastName,
 	salary
 	)
 	values (
-	2,
 	"yassir",
 	"noam",
 	100
 );
 
-insert into employee( empid, firstName, lastName, salary, dep) values ( 3, "yassin", "noam", 200, "phisi");
+insert into employee(firstName, lastName, salary, dep) values ("yassin", "noam", 200, "phisi");
 
-insert into employee( empid, firstName, lastName, salary, dep) values ( 4, "hamza", "noam", 400, "sodo");
+insert into employee(firstName, lastName, salary, dep) values ( "hamza", "noam", 400, "sodo");
 
 select * from employee; -- show the the content of the employee table
 

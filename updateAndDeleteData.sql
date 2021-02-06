c8a1021d6fa75c12ce06cf6c7f1107f5f9c960b5
yssefnoam <2020noam@gmail.com>
2021-02-06 17:38:53 +0000

    a

---
+++ updateAndDeleteData.sql
@@ -0,0 +1,9 @@
+use company;
+
+-- update employee set salary=150.00,dep="mathmatique"
+-- where firstName = "youssef";
+select * from employee;
+
+delete from employee where firstName like "h%";
+
+select * from employee;
